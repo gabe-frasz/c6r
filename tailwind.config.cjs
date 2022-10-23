e686b518ae114ef364b19a0c6e48a1fad3dd84e4
Gabriel <gabrielvitor.frasao@gmail.com>
2022-10-23 18:54:14 -0300

    refactor: add design system tokens and font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: 14,
+      base: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7c7c8a",
+        200: "#c4c4cc",
+        100: "#e1e1e6",
+      },
+      cyan: {
+        500: "#81d8f7",
+        300: "#9be1fb",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
