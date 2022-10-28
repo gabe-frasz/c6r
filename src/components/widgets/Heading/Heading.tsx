aa51370a41b46a73bcb283ba29c889981ffd5298
Gabriel <gabrielvitor.frasao@gmail.com>
2022-10-28 19:45:19 -0300

    refactor: add className prop

--- src/components/widgets/Heading/Heading.tsx
+++ src/components/widgets/Heading/Heading.tsx
@@ -4,20 +4,30 @@ import { ReactNode } from "react";
 
 export interface HeadingProps {
   size?: "sm" | "base" | "lg";
+  className?: string;
   asChild?: boolean;
   children: ReactNode;
 }
 
-export const Heading = ({ size = "base", asChild, children }: HeadingProps) => {
+export const Heading = ({
+  size = "base",
+  className = "",
+  asChild = false,
+  children,
+}: HeadingProps) => {
   const Comp = asChild ? Slot : "h2";
 
   return (
     <Comp
-      className={c("text-gray-100 font-bold font-sans", {
-        "text-lg": size === "sm",
-        "text-xl": size === "base",
-        "text-2xl": size === "lg",
-      })}
+      className={c(
+        "text-gray-100 font-bold font-sans",
+        {
+          "text-lg": size === "sm",
+          "text-xl": size === "base",
+          "text-2xl": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Comp>
--- src/components/widgets/Text/Text.tsx
+++ src/components/widgets/Text/Text.tsx
@@ -4,20 +4,30 @@ import { ReactNode } from "react";
 
 export interface TextProps {
   size?: "sm" | "base" | "lg";
+  className?: string;
   asChild?: boolean;
   children: ReactNode;
 }
 
-export const Text = ({ size = "base", asChild, children }: TextProps) => {
+export const Text = ({
+  size = "base",
+  className = "",
+  asChild = false,
+  children,
+}: TextProps) => {
   const Comp = asChild ? Slot : "span";
 
   return (
     <Comp
-      className={c("text-gray-100 font-sans", {
-        "text-sm": size === "sm",
-        "text-base": size === "base",
-        "text-md": size === "lg",
-      })}
+      className={c(
+        "text-gray-100 font-sans",
+        {
+          "text-sm": size === "sm",
+          "text-base": size === "base",
+          "text-md": size === "lg",
+        },
+        className
+      )}
     >
       {children}
     </Comp>
