e6444d15e3f599e69da141ef553bd831ccbde785
Gabriel <gabrielvitor.frasao@gmail.com>
2022-10-28 19:44:47 -0300

    refactor: add className prop and fix padding

--- src/components/widgets/Button/Button.tsx
+++ src/components/widgets/Button/Button.tsx
@@ -3,20 +3,27 @@ import c from "clsx";
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
   children: ReactNode;
   asChild?: boolean;
 }
 
-export const Button = ({ asChild, children, ...props }: ButtonProps) => {
+export const Button = ({
+  className,
+  asChild,
+  children,
+  ...props
+}: ButtonProps) => {
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
       {...props}
       className={c(
-        "w-full px-3 py-4 bg-cyan-500 text-black text-sm font-semibold rounded transition-colors",
+        "w-full px-4 py-3 bg-cyan-500 text-black text-sm font-semibold rounded transition-colors",
         "hover:bg-cyan-300",
-        "focus:ring-2 focus:ring-cyan-300 focus:ring-offset-2 focus:ring-offset-gray-900 focus:outline-none"
+        "focus:ring-2 focus:ring-cyan-300 focus:ring-offset-2 focus:ring-offset-gray-900 focus:outline-none",
+        className
       )}
     >
       {children}
