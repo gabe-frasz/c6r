e1d03b2cbcfee3e146966296595d3465dbbac271
Gabriel <gabrielvitor.frasao@gmail.com>
2022-10-28 19:46:33 -0300

    refactor: add new color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
       "2xl": 32,
     },
     colors: {
+      transparent: "transparent",
       black: "#000000",
       white: "#ffffff",
       gray: {
@@ -31,5 +32,6 @@ module.exports = {
       },
     },
   },
+  darkMode: "class",
   plugins: [],
 };
