5f243116021e984fba89dd71deaab56d689b9572
Gabriel VS Frasão <gabrielvitor.frasao@gmail.com>
2022-10-29 09:44:16 -0300

    docs: add App stories file

---
+++ src/App.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { App } from "./App";
+
+export default {
+  title: "Pages/Home",
+  component: App,
+  args: {},
+  argTypes: {},
+} as Meta;
+
+export const Default: StoryObj = {};
