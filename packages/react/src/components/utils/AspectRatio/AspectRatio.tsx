fc07a1b6d1859dd833749a9ae252b19590fd9263
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-28 02:03:48 -0300

    feat: create aspect ratio component

---
+++ packages/react/src/components/utils/AspectRatio/AspectRatio.tsx
@@ -0,0 +1,11 @@
+import * as Primitive from "@radix-ui/react-aspect-ratio";
+import { forwardRef } from "react";
+
+interface AspectRatioProps extends Primitive.AspectRatioProps {}
+
+export const AspectRatio = forwardRef<HTMLDivElement, AspectRatioProps>(
+  (props, ref) => {
+    return <Primitive.Root {...props} ref={ref} />;
+  },
+);
+AspectRatio.displayName = "AspectRatio";
