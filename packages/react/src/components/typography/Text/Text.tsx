bf61aa75be23a24d3e332977a8f529c5610ca8a2
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-28 02:03:25 -0300

    refactor: simplify component

---
+++ packages/react/src/components/typography/Text/Text.tsx
@@ -0,0 +1,26 @@
+import { Slot } from "@radix-ui/react-slot";
+import { VariantProps } from "class-variance-authority";
+import { forwardRef, HTMLAttributes } from "react";
+
+import { c } from "@/utils";
+import { text } from "./Text.styles";
+
+export interface TextProps
+  extends HTMLAttributes<HTMLElement>,
+    VariantProps<typeof text> {
+  asChild?: boolean;
+}
+
+export const Text = forwardRef<HTMLElement, TextProps>(
+  ({ size, weight, ...props }, ref) => {
+    const Comp = props.asChild ? Slot : "span";
+
+    return (
+      <Comp ref={ref} className={c(text({ size, weight }), props.className)}>
+        {props.children}
+      </Comp>
+    );
+  },
+);
+
+Text.displayName = "Text";
