f4052259658b14950b26fd09107c37081a040e6e
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-28 01:49:52 -0300

    refactor: separate components into folders

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,11 +1,2 @@
-export * from "./components/Avatar";
-export * from "./components/Button";
-export * from "./components/Checkbox";
-export * from "./components/Heading";
-export * from "./components/Input";
-export * from "./components/Label";
-export * from "./components/Modal";
-export * from "./components/Popover";
-export * from "./components/Progress";
-export * from "./components/Text";
+export * from "./components";
 export { c } from "./utils";
