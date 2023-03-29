32ad71705128e7f2b1ba4ee264663b75755ee623
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-29 15:13:20 -0300

    refactor: change size variants

--- packages/react/src/components/form/Switch/Switch.styles.ts
+++ packages/react/src/components/form/Switch/Switch.styles.ts
@@ -4,7 +4,7 @@ export const switchRoot = cva(
   "bg-base-200 rounded-full relative outline-none transition-colors disabled:opacity-50",
   {
     variants: {
-      size: { sm: "w-10 h-5", md: "w-12 h-6", lg: "w-16 h-8" },
+      size: { sm: "w-8 h-4", md: "w-10 h-5", lg: "w-12 h-6", xl: "w-16 h-8" },
       theme: {
         primary: "data-[state=checked]:bg-primary-500",
         secondary: "data-[state=checked]:bg-secondary-500",
@@ -23,9 +23,10 @@ export const switchThumb = cva(
   {
     variants: {
       size: {
-        sm: "w-4 h-4 translate-x-1 data-[state=checked]:translate-x-5",
-        md: "w-5 h-5 translate-x-1 data-[state=checked]:translate-x-6",
-        lg: "w-7 h-7 translate-x-1 data-[state=checked]:translate-x-8",
+        sm: "w-3 h-3 translate-x-1 data-[state=checked]:translate-x-4",
+        md: "w-4 h-4 translate-x-1 data-[state=checked]:translate-x-5",
+        lg: "w-5 h-5 translate-x-1 data-[state=checked]:translate-x-6",
+        xl: "w-7 h-7 translate-x-1 data-[state=checked]:translate-x-8",
       },
     },
     defaultVariants: {
