6ebe6ad59eeabf728331a256700295023915ec8c
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-29 21:48:37 -0300

    feat: create Spinner

---
+++ packages/react/src/components/data-display/Spinner/Spinner.tsx
@@ -0,0 +1,18 @@
+import { CircleNotch, type IconProps } from "phosphor-react";
+import { forwardRef } from "react";
+
+import { c } from "@/utils";
+
+export interface SpinnerProps extends IconProps {}
+
+export const Spinner = forwardRef<SVGSVGElement, SpinnerProps>((props, ref) => {
+  return (
+    <CircleNotch
+      weight="bold"
+      {...props}
+      ref={ref}
+      className={c("animate-spin", props.className)}
+    />
+  );
+});
+Spinner.displayName = "Spinner";
