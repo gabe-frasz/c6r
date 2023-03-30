e3e6dfe26f67964cabcc3db391e96b9efae6ca0e
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-29 21:50:49 -0300

    style: remove return key word

--- packages/react/src/components/utils/VisuallyHidden/VisuallyHidden.tsx
+++ packages/react/src/components/utils/VisuallyHidden/VisuallyHidden.tsx
@@ -2,6 +2,6 @@ import * as Primitive from "@radix-ui/react-visually-hidden";
 
 interface VisuallyHiddenProps extends Primitive.VisuallyHiddenProps {}
 
-export const VisuallyHidden = (props: VisuallyHiddenProps) => {
-  return <Primitive.Root {...props} />;
-};
+export const VisuallyHidden = (props: VisuallyHiddenProps) => (
+  <Primitive.Root {...props} />
+);
