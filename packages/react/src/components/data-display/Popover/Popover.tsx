0f97aa67503ef55c58e31ee61dc1969257cc1c40
Gabriel <gabrielvitor.frasao@gmail.com>
2023-03-30 01:17:33 -0300

    refactor: add enter/leave transition

--- packages/react/src/components/data-display/Popover/Popover.tsx
+++ packages/react/src/components/data-display/Popover/Popover.tsx
@@ -1,13 +1,27 @@
 "use client";
 
+import { Transition } from "@headlessui/react";
 import * as Primitive from "@radix-ui/react-popover";
-import { forwardRef } from "react";
+import { forwardRef, Fragment, useContext } from "react";
 
 import { c } from "@/utils";
+import { PopoverContext } from "./PopoverContext";
 
-export interface PopoverProps extends Primitive.PopoverProps {}
+export interface PopoverProps extends Primitive.PopoverProps {
+  open: boolean;
+}
 
-const Root = (props: PopoverProps) => <Primitive.Root {...props} />;
+const Root = (props: PopoverProps) => {
+  const context = {
+    open: props.open,
+  };
+
+  return (
+    <PopoverContext.Provider value={context}>
+      <Primitive.Root {...props} />
+    </PopoverContext.Provider>
+  );
+};
 
 export interface PopoverTriggerProps extends Primitive.PopoverTriggerProps {}
 
@@ -22,20 +36,34 @@ export interface PopoverContentProps extends Primitive.PopoverContentProps {}
 
 const Content = forwardRef<HTMLDivElement, PopoverContentProps>(
   (props, ref) => {
-    return (
-      <Primitive.Content
-        sideOffset={5}
-        {...props}
-        ref={ref}
-        className={c(
-          "w-screen max-w-md bg-base-200 fill-base-200 p-3 rounded shadow text-base-content z-50 sm:w-fit",
-          props.className,
-        )}
-      >
-        <Primitive.Arrow className="fill-inherit" />
+    const { open } = useContext(PopoverContext);
 
-        {props.children}
-      </Primitive.Content>
+    return (
+      <Transition
+        as={Fragment}
+        show={open}
+        unmount={false}
+        enter="ease-out duration-100"
+        enterFrom="opacity-0 translate-y-1"
+        enterTo="opacity-100 translate-y-0"
+        leave="ease-in duration-100"
+        leaveTo="opacity-0 translate-y-1"
+      >
+        <Primitive.Content
+          sideOffset={5}
+          {...props}
+          ref={ref}
+          forceMount
+          className={c(
+            "w-screen max-w-md bg-base-200 fill-base-200 p-3 rounded shadow text-base-content z-50 sm:w-fit",
+            props.className,
+          )}
+        >
+          <Primitive.Arrow className="fill-inherit" />
+
+          {props.children}
+        </Primitive.Content>
+      </Transition>
     );
   },
 );
