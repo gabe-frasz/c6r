890ccdd48dcb38e67ae010830e25272e5ed8e3c8
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-01 09:42:25 -0300

    fix: add use client directive

--- packages/react/src/components/data-display/Alert/Alert.tsx
+++ packages/react/src/components/data-display/Alert/Alert.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Transition } from "@headlessui/react";
 import * as Primitive from "@radix-ui/react-alert-dialog";
 import { forwardRef, Fragment, useContext } from "react";
