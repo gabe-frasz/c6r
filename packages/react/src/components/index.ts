814cb01f6bbd8e13a9b6ef23c3f3af6faa1ef7e6
Gabriel <gabrielvitor.frasao@gmail.com>
2023-04-17 18:39:32 -0300

    fix(react): use named exports instead of star

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,20 +1,80 @@
-export * from "./data-display/Alert";
-export * from "./data-display/Avatar";
-export * from "./data-display/Modal";
-export * from "./data-display/Popover";
-export * from "./data-display/Progress";
-export * from "./data-display/Spinner";
-export * from "./data-display/Tooltip";
-export * from "./form/Button";
-export * from "./form/Checkbox";
-export * from "./form/Input";
-export * from "./form/Label";
-export * from "./form/RadioGroup";
-export * from "./form/Select";
-export * from "./form/Switch";
-export * from "./typography/Heading";
-export * from "./typography/Text";
-export * from "./utils/AspectRatio";
-export * from "./utils/Divider";
-export * from "./utils/Skeleton";
-export * from "./utils/VisuallyHidden";
+export { Alert } from "./data-display/Alert";
+export type {
+  AlertActionProps,
+  AlertCancelProps,
+  AlertContentProps,
+  AlertDescriptionProps,
+  AlertProps,
+  AlertTitleProps,
+  AlertTriggerProps,
+} from "./data-display/Alert";
+export { Avatar } from "./data-display/Avatar";
+export type {
+  AvatarFallbackProps,
+  AvatarImageProps,
+  AvatarProps,
+} from "./data-display/Avatar";
+export { Modal } from "./data-display/Modal";
+export type {
+  ModalContentProps,
+  ModalDescriptionProps,
+  ModalProps,
+  ModalTitleProps,
+  ModalTriggerProps,
+} from "./data-display/Modal";
+export { Popover } from "./data-display/Popover";
+export type {
+  PopoverContentProps,
+  PopoverProps,
+  PopoverTriggerProps,
+} from "./data-display/Popover";
+export { Progress } from "./data-display/Progress";
+export type {
+  ProgressIndicatorProps,
+  ProgressProps,
+} from "./data-display/Progress";
+export { Spinner } from "./data-display/Spinner";
+export type { SpinnerProps } from "./data-display/Spinner";
+export { Tooltip } from "./data-display/Tooltip";
+export type {
+  TooltipContentProps,
+  TooltipProps,
+  TooltipProviderProps,
+  TooltipTriggerProps,
+} from "./data-display/Tooltip";
+export { Button } from "./form/Button";
+export type { ButtonProps } from "./form/Button";
+export { Checkbox } from "./form/Checkbox";
+export type { CheckboxProps } from "./form/Checkbox";
+export { Input } from "./form/Input";
+export type { InputFieldProps, InputIconProps, InputProps } from "./form/Input";
+export { Label } from "./form/Label";
+export type { LabelProps } from "./form/Label";
+export { RadioGroup } from "./form/RadioGroup";
+export type {
+  RadioGroupIndicatorProps,
+  RadioGroupItemProps,
+  RadioGroupProps,
+} from "./form/RadioGroup";
+export { Select } from "./form/Select";
+export type {
+  SelectContentProps,
+  SelectGroupProps,
+  SelectItemProps,
+  SelectProps,
+  SelectTriggerProps,
+} from "./form/Select";
+export { Switch } from "./form/Switch";
+export type { SwitchProps, SwitchThumbProps } from "./form/Switch";
+export { Heading } from "./typography/Heading";
+export type { HeadingProps } from "./typography/Heading";
+export { Text } from "./typography/Text";
+export type { TextProps } from "./typography/Text";
+export { AspectRatio } from "./utils/AspectRatio";
+export type { AspectRatioProps } from "./utils/AspectRatio";
+export { Divider } from "./utils/Divider";
+export type { DividerProps } from "./utils/Divider";
+export { Skeleton, SkeletonText } from "./utils/Skeleton";
+export type { SkeletonProps, SkeletonTextProps } from "./utils/Skeleton";
+export { VisuallyHidden } from "./utils/VisuallyHidden";
+export type { VisuallyHiddenProps } from "./utils/VisuallyHidden";
