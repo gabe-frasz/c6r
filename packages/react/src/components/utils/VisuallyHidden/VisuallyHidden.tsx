3ccf5fdafae6248375a79779e320a5626c60c888
Gabriel <gabrielvitor.frasao@gmail.com>
2023-05-03 21:44:32 -0300

    refactor(react): remove components

--- packages/react/src/components/utils/VisuallyHidden/VisuallyHidden.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import * as Primitive from "@radix-ui/react-visually-hidden";
-
-export interface VisuallyHiddenProps extends Primitive.VisuallyHiddenProps {}
-
-export const VisuallyHidden = (props: VisuallyHiddenProps) => (
-  <Primitive.Root {...props} />
-);
