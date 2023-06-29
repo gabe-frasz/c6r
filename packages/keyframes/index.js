29e5cb046c5bc5e19d6e006ad77c13e510c8790d
Gabriel <gabrielvitor.frasao@gmail.com>
2023-06-29 20:46:30 -0300

    refactor(keyframes): update animations

--- packages/keyframes/index.js
+++ packages/keyframes/index.js
@@ -3,80 +3,43 @@ module.exports = {
     extend: {
       keyframes: {
         openDialogMobile: {
-          "0%": {
-            opacity: 0,
-            transform: "translateY(100%)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translateY(0)",
-          },
+          "0%": { opacity: 0, transform: "translateY(100%)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
         },
         closeDialogMobile: {
-          "0%": {
-            opacity: 1,
-            transform: "translateY(0)",
-          },
-          "100%": {
-            opacity: 0,
-            transform: "translateY(100%)",
-          },
+          "0%": { opacity: 1, transform: "translateY(0)" },
+          "100%": { opacity: 0, transform: "translateY(100%)" },
         },
         openDialogDesktop: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-50%, -48%) scale(0.96)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%, -50%) scale(1)",
-          },
+          "0%": { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
+          "100%": { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
         },
         closeDialogDesktop: {
-          "0%": {
-            opacity: 1,
-            transform: "translate(-50%, -50%) scale(1)",
-          },
+          "0%": { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
           "100%": {
             opacity: 0,
             transform: "translate(-50%, -48%) scale(0.96)",
           },
         },
-        openDialogOverlay: {
-          "0%": {
-            opacity: 0,
-          },
-          "100%": {
-            opacity: 1,
-          },
+        slideUpAndFade: {
+          "0%": { opacity: 0, transform: "translateY(2px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        closeDialogOverlay: {
-          "0%": {
-            opacity: 1,
-          },
-          "100%": {
-            opacity: 0,
-          },
+        slideRightAndFade: {
+          "0%": { opacity: 0, transform: "translateX(-2px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
         },
-        openPopover: {
-          "0%": {
-            opacity: 0,
-            transform: "translateY(0.25rem)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translateY(0)",
-          },
+        slideDownAndFade: {
+          "0%": { opacity: 0, transform: "translateY(-2px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
         },
-        closePopover: {
-          "0%": {
-            opacity: 1,
-            transform: "translateY(0)",
-          },
-          "100%": {
-            opacity: 0,
-            transform: "translateY(0.25rem)",
-          },
+        slideLeftAndFade: {
+          "0%": { opacity: 0, transform: "translateX(2px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+        closePopup: {
+          "0%": { opacity: 1, transform: "scale(1)" },
+          "100%": { opacity: 0, transform: "scale(0.98)" },
         },
       },
       animation: {
@@ -84,12 +47,11 @@ module.exports = {
         "close-dialog-mobile": "closeDialogMobile 100ms ease-in",
         "open-dialog-desktop": "openDialogDesktop 150ms ease-out",
         "close-dialog-desktop": "closeDialogDesktop 100ms ease-in",
-        "open-dialog-overlay": "openDialogOverlay 100ms",
-        "close-dialog-overlay": "closeDialogOverlay 100ms",
-        "open-popover": "openPopover 100ms ease-out",
-        "close-popover": "closePopover 100ms ease-in",
-        "open-tooltip": "openPopover 100ms ease-out",
-        "close-tooltip": "closePopover 100ms ease-in",
+        "open-popup-top": "slideUpAndFade 300ms ease-out",
+        "open-popup-right": "slideRightAndFade 300ms ease-out",
+        "open-popup-bottom": "slideDownAndFade 300ms ease-out",
+        "open-popup-left": "slideLeftAndFade 300ms ease-out",
+        "close-popup": "closePopup 100ms ease-in",
       },
     },
   },
